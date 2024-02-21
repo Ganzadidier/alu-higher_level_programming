ab8b6d4489b6e45192c575f37ad1474bcc2110e4
Ganzadidier <d.ganza@alustudent.com>
2024-02-21 07:07:51 -0800

    task6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+for i in range(10):
+	for j in range(10):
+	    
+	    if i==j:
+	        continue
+	    elif i>j:
+	        continue
+	    elif i==8 and j==9:
+	        continue
+	    else:
+	        print("{}{}".format(i,j), end=', ')
+	        
+print(89)
