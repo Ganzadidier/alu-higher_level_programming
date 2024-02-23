84c3795257e23fe108ca7cddeb5c4fc0071575eb
Ganzadidier <d.ganza@alustudent.com>
2024-02-23 05:35:38 -0800

    changing task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    translate_table = str.maketrans('','', 'cC')
-    translated_string = my_string.translate(translate_table)
-    return translated_string
+
+
+
+    for i in my_string:
+
+        if i =='c' or i == 'C':
+            continue
+        print(i,end='')
