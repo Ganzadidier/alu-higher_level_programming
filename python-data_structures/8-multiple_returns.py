ea47fe4bd919669bfe0483272d63ce4bc50399c9
Ganzadidier <d.ganza@alustudent.com>
2024-02-23 16:16:52 -0800

    task9

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     count = 0
 
     if len(sentence) == 0:
-        zero_tuple = (0,None)
+        zero_tuple = (0, None)
         return zero_tuple
     else:
         len_char_tuple = (len(sentence), sentence[0])
