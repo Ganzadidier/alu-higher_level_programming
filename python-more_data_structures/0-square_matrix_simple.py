6e7cbdd4d5b80bd821436232e03f7ce46164801a
Ganzadidier <d.ganza@alustudent.com>
2024-02-27 05:21:10 -0800

    task0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_copy = matrix.copy()
-    for i in matrix_copy:
-        for j in i:
-            j ** 2
+    matrix_copy = []
+    for i in matrix:
+	    matrix_copy.append(i)
+	    for j in range(len(i)):
+	        i[j] = i[j]**2
     return matrix_copy
